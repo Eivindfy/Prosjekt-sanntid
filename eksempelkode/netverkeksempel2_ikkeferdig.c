3ac4fc55787b8e526cc159360b7b26c6a1dbc97c
Eivindfy <eivindfylkesnes@hotmail.com>
2014-01-24 15:58:21 +0100

    Update netverkeksempel2_ikkeferdig.c
    Trenger bare å stenge sockete/porten skikkelig eventuelt la porten være reusable.

--- eksempelkode/netverkeksempel2_ikkeferdig.c
+++ eksempelkode/netverkeksempel2_ikkeferdig.c
@@ -7,21 +7,23 @@
 #include <netdb.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 #define PORT 33546
 #define HOST "129.241.187.161"
-#define MY_IP "129.241.187.143"
-#define MY_PORT 10
+#define MY_IP "129.241.187.150"
+#define MY_PORT 20000
 
 
-void* accept(sockaddr_in local_addr,int socket2fd){
-	socklen_t addr_size = sizeof local_addr;
-        if(accept(socket2fd, (struct sockaddr* )local_addr, &addr_size)==-1){
-                printf("error");
-                return -1;
-        }
 
-}
+
+//void* accept_thread(void * arrg){
+//	socklen_t addr_size = sizeof local_addr;
+//        if(accept(socket2fd, (struct sockaddr* )local_addr, &addr_size)==-1){
+//                printf("error");
+//                return -1;
+//        }
+//}
 
 
 int main(){
@@ -38,7 +40,7 @@ int main(){
 	
 	struct in_addr host_addr;
 	if(inet_pton(AF_INET, HOST,&host_addr) <=0){
-		printf("error");
+		printf("error creating host address");
 		return -1;
 	} 	
 	server_addr->sin_port = htons(PORT);
@@ -46,13 +48,17 @@ int main(){
 	server_addr->sin_family = AF_INET;
 	
 	if(connect(socketfd, (struct sockaddr* )server_addr,sizeof(*server_addr)) == -1){
-		printf("error");
+		printf("error connecting to server");
 		return -1;
 	}
 	char buf[1024];
 	recv(socketfd, buf, sizeof(buf), 0);
-	printf("%s\n", buf);
 
+	printf(":%s\n:", buf);
+
+	
+	
+	
 	struct sockaddr_in *local_addr;
         struct sockaddr_in temp2;
         local_addr = &temp2;
@@ -60,7 +66,7 @@ int main(){
 
 	socket2fd = socket(AF_INET, SOCK_STREAM,0);
 
-	if(socketfd== -1){
+	if(socket2fd== -1){
                 printf("error opening socket");
                 return -1;
         }
@@ -76,25 +82,46 @@ int main(){
 
 
 
-	char* message = "Connect to: 129.241.187.143:33546\0";
+
+	char* message = "Connect to: 129.241.187.150:20000\0";
 	send(socketfd,message,strlen(message)+1,0);
-	
+
+
+
+	if(bind(socket2fd, (struct sockaddr* ) local_addr, sizeof(*local_addr))==-1){
+		printf("error binding socket: %i",errno);
+		return -1;
+	}
 	listen(socket2fd,0);
 	
 
-	pthread_t accept_thr;
-	pthread_create(&accept_thr,
-//	socklen_t addr_size = sizeof local_addr;
-//	if(accept(socket2fd, (struct sockaddr* )local_addr, &addr_size)==-1){
-//		printf("error");
-//		return -1;
-//	}
-		
-	//send(socket2fd,message,strlen(message)+1,0);
+//	pthread_t accept_thr;
+//	pthread_create(&accept_thr,
+
+	int new_sock;
+
+	socklen_t addr_size = sizeof local_addr;
+
+	new_sock=accept(socket2fd, (struct sockaddr* )local_addr, &addr_size);
+	
+
+	
+	if(new_sock ==-1){
+		printf("error");
+		return -1;
+	}
+
+	recv(new_sock, buf, sizeof(buf), 0);
+	printf("%s\n", buf);
+
+	message = "Hei ny melding!";	
+	send(new_sock,message,strlen(message)+1,0);
 
 
-	recv(socket2fd, buf, sizeof(buf), 0);
+	recv(new_sock, buf, sizeof(buf), 0);
         printf("%s\n", buf);
+	
+	shutdown(socket2fd,2);		
 
 	return 0;
 }

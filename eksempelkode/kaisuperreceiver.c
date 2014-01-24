6a76f2d9f61f849f0311327a4e4da3da25b7b436
kairobert <kaisemailaddress@gmail.com>
2014-01-24 15:31:01 +0100

    Update kaisuperreceiver.c

--- eksempelkode/kaisuperreceiver.c
+++ eksempelkode/kaisuperreceiver.c
@@ -1,42 +1,52 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define PORT 1234
-#define ADDRESS "127.0.0.1"
-
+#define PORT 34933
+#define ADDRESS "129.241.187.161"
 
 
 
 int main(){
-int descriptor=0;
 
-fd = socket(AF_INET,SOCK_STREAM,0);  //PF_INET ???????
+
+int fd = socket(AF_INET,SOCK_STREAM,0);  //PF_INET ???????
 struct sockaddr_in address;
 
 address.sin_family = AF_INET;
 address.sin_port = htons(PORT);
-inet_pton(AF_INET, "ADDRESS", &address.sin_addr)
+
+//lager AF_INET addresse som pc forstaar ut av ADDRESS og legger inn i address.sin_addr
+inet_pton(AF_INET, ADDRESS, &address.sin_addr);
 
 
 
- 
+
 if (-1 == connect(fd, (struct sockaddr *)&address, sizeof(address)))
     {
       perror("connect failed");
-      close(SocketFD);
+      close(fd);
       exit(EXIT_FAILURE);
     }
-    
-    
-//gjer noko
-    
+
+char buffer[1024];
+int numberofchar;
+
+
+
+numberofchar=read(fd,buffer,1024);
+for (int i=0;i < numberofchar; i++){
+printf("%c",buffer[i]);
+}
+
+close(fd);
+
 
 return 0;
 }
-

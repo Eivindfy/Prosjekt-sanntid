ad05428c7644d996aed1b8b7da58b602f3dc9eac
kairobert <kaisemailaddress@gmail.com>
2014-02-27 12:00:55 +0100

    Update tcpudpchannel.c
    fiksa file descriptor-problemet

--- eksempelkode/test av tcpudpchannel/tcpudpchannel.c
+++ eksempelkode/test av tcpudpchannel/tcpudpchannel.c
@@ -6,25 +6,20 @@
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 #include "tcpudpchannel.h"
 
 
 // erstatt desse her med ei headerfil med  definisjonar eller noko
 #define UDPADDRESS "127.0.0.1"
-#define UDPPORT 1234
+#define UDPPORT 12345
 #define TCPADDRESS "127.0.0.1"
-#define TCPPORT 1243
+#define TCPPORT 12435
 #define BUFFERLENGTH 1024
 //
 
-// VIRKER IKKJE SANNSYNELIGVIS
-//
-//
-//
-//
-// MÅ NOK BINDE TCPSOCKET FØR DU BRUKER DENNE FUNKSJONEN
-//
+
 // SEND PÅ SAMME UDPADDRESSE OG PORT ALLTID????
 
 int initializetcpudpchannel(){
@@ -34,22 +29,34 @@ int initializetcpudpchannel(){
 
 	//printf("socketpair created\n");
 	
+	
+	int* dynamic_int_pointer =  malloc(sizeof(*dynamic_int_pointer));
+	*dynamic_int_pointer = fd[0];
+	void* dynamic_void_pointer;
+	dynamic_void_pointer = (void*)dynamic_int_pointer;
+	
+	
 	pthread_t channel;
 	printf("This is the file descriptor sent to the channel: %i \n", fd[0]);
-	pthread_create(&channel,NULL, tcpudpchannel, (void *)&fd[0]);
+	pthread_create(&channel,NULL, tcpudpchannel, dynamic_void_pointer);
 	printf("done creating thread in initialize\n");
 	return fd[1];
 
 }
 
 
-void *tcpudpchannel(void * tcp_socketfdvoid){
 
-printf("tcpudpchannel thread created\n  this is the tcp void file descriptor: %i \n", tcp_socketfdvoid);
 
-int tcp_socketfd = * ((int*) tcp_socketfdvoid);
 
-printf("this is the tcp int file descriptor: %i \n", tcp_socketfd);
+
+
+
+void *tcpudpchannel(void* tcp_socketfdvoid){
+
+printf("tcpudpchannel thread created\n");
+
+int tcp_socketfd = *((int*)tcp_socketfdvoid);
+free(tcp_socketfdvoid);
 
 char buffer[BUFFERLENGTH];
 int transferstatus;
@@ -88,9 +95,8 @@ printf("fd_max complete \n");
 fd_set master_set;
 printf("master_set created\n");
 FD_ZERO(&master_set);
-printf("master_set used as input in FD_ZERO\n    tcp_socketfd is: %i \n udp_socketfd is: %i \n",tcp_socketfd,udp_socketfd);
+printf("master_set used as input in FD_ZERO\n- tcp_socketfd is: %i \n- udp_socketfd is: %i \n",tcp_socketfd,udp_socketfd);
 FD_SET(tcp_socketfd,&master_set);
-printf("put tcp_socketfd in master_set\n");
 FD_SET(udp_socketfd,&master_set);
 //
 struct timeval tv;
@@ -102,40 +108,46 @@ while(1){
 	select(fd_max,&master_set,NULL,NULL,&tv);
 
 	if( FD_ISSET(tcp_socketfd,&master_set)){
-
+        
+        
+        printf("CHANNEL: receiving from TCP\n");
 		transferstatus = recv(tcp_socketfd,buffer,(int)sizeof(buffer),0);
-		// printf("test: %s \n",buffer);
+		printf("CHANNEL: received from TCP, message is: %s \n",buffer);
 		if (transferstatus<0){
 			perror("Failure receiving from TCP in tcpudpchannel\n");
-			
 		}
-
+		
+        
+        printf("CHANNEL: sending to UDP\n");
 		transferstatus=sendto(udp_socketfd,buffer,sizeof(buffer),0,(struct 			 			sockaddr*)&udp_socket_address,udp_address_length);
+		printf("CHANNEL: sent to UDP\n");
 		if (transferstatus<0){
 			perror("Failure sending to UDP in tcpudpchannel\n");
-			
-	  }
+	    }
+	
 
 	}
 
 	else if( FD_ISSET(udp_socketfd,&master_set)){
-		printf("this is the buffer before receiving from udp: %s , size of it: %i \n",buffer,sizeof(buffer));
+	//	printf("this is the buffer before receiving from udp: %s , size of it: %i \n",buffer,sizeof(buffer));
 
+
+        printf("CHANNEL: Receiving from UDP\n");
 		transferstatus=recvfrom(udp_socketfd,buffer,sizeof(buffer),0,(struct 					  sockaddr*)&udp_socket_address, &udp_address_length);
+		printf("CHANNEL: Received from UDP, message is: %s \n", buffer);
 		if ( transferstatus < 0 ){
 			perror("Failure receiving from UDP in tcpudpchannel\n");
-			
 		}
+	
 
 		
-
+        printf("CHANNEL: Sending to TCP\n");
 		transferstatus=send(tcp_socketfd,buffer,sizeof(buffer),0);
+        printf("CHANNEL: Sent to TCP\n");
 		if( transferstatus < 0 ){
 			perror("Failure sending to TCP in tcpudpchannel\n");
-		
 		}
-
+ 
 	}
 }
 }
-

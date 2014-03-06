e329191329328fc5c36daf56ba110af4f39f88df
Tor-Christian <trofast@gmail.com>
2014-03-06 18:05:02 +0100

    Inkluderte de globale variablene i control functions

--- driver/README.md
+++ driver/README.md
@@ -1,1 +1,9 @@
 På FLOOR_STACK må eg legge til ein funksjon som inserten flooren inn på rett plass.
+
+Reserverte coder for sending av beskjeder mellom tråder:
+destination reached = 'r'
+floor visited = 'v'
+down button pressed = 'd'
+up button pressed = 'u'
+command button pressed = 'c'
+stop button pressed = 's'
--- driver/buttons.c
+++ driver/buttons.c
@@ -6,6 +6,7 @@
 #include "elev.h"
 #include "buttons.h"
 #include "utility_functions.h"
+#include "global_variables.h"
 
 int button_initialize(){
 	int fd[2];
@@ -27,33 +28,53 @@ int button_initialize(){
 void* button_return(void * socket_fd_void){
 	int socket_fd = * (int *) socket_fd_void;
 	free(socket_fd_void);
+	int down_button[N_FLOORS];
+	int up_button[N_FLOORS];
+	int command_button[N_FLOORS];
 	
+	for(int i = 0; i < N_FLOORS; i++){
+		down_button[i] = 0;
+		up_button[i] = 0;
+		command_button[i] = 0;
+	}
 	
 	char send_buffer[1024];
 	send_buffer[5]='\0';
 	while(1){
 		for (int i = 1; i < N_FLOORS; i++){
-			if (elev_get_button_signal(BUTTON_CALL_DOWN, i)){
+			if (elev_get_button_signal(BUTTON_CALL_DOWN, i) && !down_button[i]){
 				send_buffer[0]='d';
 				insert_floor_into_buffer(i,send_buffer);
 				send(socket_fd, send_buffer, sizeof(send_buffer),0);
+				down_button[i] = 1;
+			}
+			else if ( (!elev_get_button_signal(BUTTON_CALL_DOWN, i)) && down_button[i]){
+				down_button[i] = 0;			
 			}
 
 		}
 		for (int i = 0; i < N_FLOORS-1; i++){
-			if (elev_get_button_signal(BUTTON_CALL_UP, i)){
+			if (elev_get_button_signal(BUTTON_CALL_UP, i) && !up_button[i]){
 				send_buffer[0] = 'u';
 				insert_floor_into_buffer(i,send_buffer);
 				send(socket_fd, send_buffer, sizeof(send_buffer), 0);
+				up_button[i] = 1;
+			}
+			else if ( (!elev_get_button_signal(BUTTON_CALL_UP, i)) && up_button[i]){
+				up_button[i] = 0;
 			}
 
 
 		}
 		for (int i = 0; i < N_FLOORS; i++){
-			if(elev_get_button_signal(BUTTON_COMMAND, i)){
+			if(elev_get_button_signal(BUTTON_COMMAND, i) && !command_button[i]){
 				send_buffer[0] = 'c';
 				insert_floor_into_buffer(i,send_buffer);
 				send(socket_fd, send_buffer, sizeof(send_buffer), 0);
+				command_button[i] = 1;
+			}
+			else if(elev_get_button_signal(BUTTON_COMMAND, i) && !command_button[i]){
+				command_button[i] = 0;
 			}
 		if(elev_get_stop_signal()){
 				send_buffer[0] = 's';

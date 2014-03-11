323db398228631cbfe760855a3a33ff04894c97a
kairobert <kaisemailaddress@gmail.com>
2014-03-11 18:26:13 +0100

    Update queue.h

--- driver/queue.h
+++ driver/queue.h
@@ -1,9 +1,18 @@
 //#include "global_variables.h"
 
 struct ORDER_QUEUE{
-  int value;
+  int floor;
   int direction;
   struct QUEUE* next;
 }
 
-struct ORDER_QUEUE first;
+struct ORDER_QUEUE* root;
+
+
+void order_queue_initialize();
+
+void order_queue_insert(int floor,int direction);
+
+void order_queue_destroy();
+
+struct order_queue order_queue_pop();

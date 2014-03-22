81e491e4c18d16f5213338abf968b77cc96c5597
FWorren <freworr@gmail.com>
2014-03-22 18:00:43 +0100

    client backup fungerer no

--- driver/testmappe/main.c
+++ driver/testmappe/main.c
@@ -1,33 +1,43 @@
-//#define MAIN_FILE
+#define MAIN_FILE
 
-//#include "global_variables.h"
+#include "global_variables.h"
 #include <stdio.h>
-#include "queue.h"
+#include "client_backup.h"
 //#include "utility_functions.h"
-//#include <string.h>
+#include <string.h>
 
 
 
 
 int main(){
 	
-	struct ORDER_QUEUE temp;
-	order_queue_initialize();
-	order_queue_destroy();
-	order_queue_initialize();
-	order_queue_insert(110,1);
-	order_queue_insert(111,1);
-	order_queue_insert(110,1);
-	order_queue_insert(110,-1);
+	initialize_global_variables();
 	
-	for(int i=0; i<6;i++){
+	global_direction = 20;
+	global_idle = 30;
+	global_destination = 40;
+	global_current_floor = 50;
 	
-	    if(!order_queue_isempty()){
-		temp=order_queue_pop();
-		printf("Iteration: %i floor: %i, direction: %i \n",i,temp.floor,temp.direction);
-		}
+	global_stop_array[0]=-5;
+	global_stop_array[1]=2;
+	global_stop_array[2]=3;
+	global_stop_array[3]=-4;
 	
-	}
+	write_backup_client("/home/student/Prosjekt-sanntid/driver/testmappe/hei.txt");
+	
+	initialize_global_variables();
+	global_direction = -8;
+	global_idle = -8;
+	global_destination = -8;
+	global_current_floor = -8;
+	
+	retrieve_backup_client("/home/student/Prosjekt-sanntid/driver/testmappe/hei.txt");
+	
+	printf("global_direction: %i, global_idle: %i, global_destination: %i, global_current_floor: %i\n", global_direction, global_idle, global_destination, global_current_floor);
+	
+	for(int i = 0 ; i < N_FLOORS ; i++){
+    	printf("%i: %i\n",i,global_stop_array[i]);
+    }
 	
 	
 	
